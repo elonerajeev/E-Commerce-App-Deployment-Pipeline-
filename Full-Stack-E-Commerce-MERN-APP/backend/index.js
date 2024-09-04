5dec1e38e9b4631605d95615df6d47ce3e3d5966
Rajeev Kumar <rajeev02030066@gmail.com>
2024-09-05 00:20:00 +0530

    Added Full-Stack-E-Commerce-MERN-APP as a directory

---
+++ Full-Stack-E-Commerce-MERN-APP/backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
+
+
+const app = express()
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api",router)
+
+app.get('/', (req, res) => {
+    res.send('Sucessfully Back-End Code Is been Started.....')
+  })
+
+const PORT = 5000 || process.env.PORT
+
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+    })
+})
