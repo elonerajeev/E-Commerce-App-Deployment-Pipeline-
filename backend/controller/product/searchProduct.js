c0d8525f39b7b7af932ed32ce7b9263eb8b3e62e
Rajeev kumar <114682224+elonerajeev@users.noreply.github.com>
2025-06-04 15:58:30 +0530

    Update searchProduct.js

--- backend/controller/product/searchProduct.js
+++ backend/controller/product/searchProduct.js
@@ -1,36 +1,69 @@
+// const productModel = require("../../models/productModel")
+
+// const searchProduct = async(req,res)=>{
+//     try{
+//         const query = req.query.q 
+
+//         const regex = new RegExp(query,'i','g')
+
+//         const product = await productModel.find({
+//             "$or" : [
+//                 {
+//                     productName : regex
+//                 },
+//                 {
+//                     category : regex
+//                 }
+//             ]
+//         })
+
+
+//         res.json({
+//             data  : product ,
+//             message : "Search Product list",
+//             error : false,
+//             success : true
+//         })
+//     }catch(err){
+//         res.json({
+//             message : err.message || err,
+//             error : true,
+//             success : false
+//         })
+//     }
+// }
+
+// module.exports = searchProduct
+
 const productModel = require("../../models/productModel")
 
-const searchProduct = async(req,res)=>{
-    try{
-        const query = req.query.q 
+const searchProduct = async (req, res) => {
+    try {
+        const query = req.query.q || "";
 
-        const regex = new RegExp(query,'i','g')
+        // Create a case-insensitive regex for searching
+        const regex = new RegExp(query, 'i');
 
         const product = await productModel.find({
-            "$or" : [
-                {
-                    productName : regex
-                },
-                {
-                    category : regex
-                }
+            "$or": [
+                { productName: regex },
+                { category: regex }
             ]
-        })
-
+        });
 
         res.json({
-            data  : product ,
-            message : "Search Product list",
-            error : false,
-            success : true
-        })
-    }catch(err){
+            data: product,
+            message: "Search Product list",
+            error: false,
+            success: true
+        });
+    } catch (err) {
         res.json({
-            message : err.message || err,
-            error : true,
-            success : false
-        })
+            message: err.message || err,
+            error: true,
+            success: false
+        });
     }
 }
 
-module.exports = searchProduct
+module.exports = searchProduct;
